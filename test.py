343da009bc297585db6abe498a5f47c4a22e2496
LSH <nupnpu3@gmail.com>
2024-11-12 17:00:35 +0900

    test change

--- test.py
+++ test.py
@@ -1,9 +1,2 @@
-print("Hello world")
-print("Hello world")
-print("Hello world")
-print("Hello world")
-print("Hello world")
-print("Hello world")
-print("Hello world")
-print("Hello world")
-print("Hello world")
+for i in range(5):
+    print("Hello World")
